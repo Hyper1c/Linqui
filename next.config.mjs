bc48bf549d4d105959048fb0559595e0ff858543
Hyper1c <salazarandres1111z@gmail.com>
2025-04-15 10:20:07 -0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ext.same-assets.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
